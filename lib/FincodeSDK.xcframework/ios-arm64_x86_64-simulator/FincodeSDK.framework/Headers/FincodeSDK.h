a1fe3ac821e251f578db461653c32020f8616549
fincode-developer <fincode-developer@gmo-pg.com>
2023-01-11 20:30:24 +0900

    xcframework追加

---
+++ lib/FincodeSDK.xcframework/ios-arm64_x86_64-simulator/FincodeSDK.framework/Headers/FincodeSDK.h
@@ -0,0 +1,18 @@
+//
+//  FincodeSDK.h
+//  FincodeSDK
+//
+//  Created by 中嶋彰 on 2022/01/11.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FincodeSDK.
+FOUNDATION_EXPORT double FincodeSDKVersionNumber;
+
+//! Project version string for FincodeSDK.
+FOUNDATION_EXPORT const unsigned char FincodeSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FincodeSDK/PublicHeader.h>
+
+
