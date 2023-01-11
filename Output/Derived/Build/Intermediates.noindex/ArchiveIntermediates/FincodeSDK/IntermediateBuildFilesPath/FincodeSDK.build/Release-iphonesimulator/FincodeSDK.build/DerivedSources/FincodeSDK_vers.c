52ee5121651fafb059cc4ec188f7cb221a6db57f
fincode-developer <fincode-developer@gmo-pg.com>
2023-01-11 21:31:13 +0900

    xcframework追加

---
+++ Output/Derived/Build/Intermediates.noindex/ArchiveIntermediates/FincodeSDK/IntermediateBuildFilesPath/FincodeSDK.build/Release-iphonesimulator/FincodeSDK.build/DerivedSources/FincodeSDK_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char FincodeSDKVersionString[];
+ extern const double FincodeSDKVersionNumber;
+
+ const unsigned char FincodeSDKVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:FincodeSDK  PROJECT:FincodeSDK-1" "\n";
+ const double FincodeSDKVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ Output/Derived/Build/Intermediates.noindex/ArchiveIntermediates/FincodeSDK/IntermediateBuildFilesPath/FincodeSDK.build/Release-iphonesimulator/FincodeSDK.build/Objects-normal/x86_64/FincodeSDK_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/nakajimaakira/work/gmo-ep/FincodeSDK/Output/Derived/Build/Intermediates.noindex/ArchiveIntermediates/FincodeSDK/IntermediateBuildFilesPath/FincodeSDK.build/Release-iphonesimulator/FincodeSDK.build/DerivedSources/FincodeSDK_vers.c
